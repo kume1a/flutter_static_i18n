93f253c6ef9cf6dd64ad5b5a2cc95536ca76aefb
kume1a <kumela011@gmail.com>
2022-06-16 18:38:18 +0400

    fix 2,15 compatibility issue

--- lib/src/static_i18n.dart
+++ lib/src/static_i18n.dart
@@ -30,6 +30,6 @@ class StaticI18N {
   static Future<void> changeLocale(Locale locale) async {
     StaticI18N.locale = locale;
 
-    await WidgetsBinding.instance.performReassemble();
+    await WidgetsBinding.instance?.performReassemble();
   }
 }
